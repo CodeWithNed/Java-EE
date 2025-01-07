f5ee69ea90eb2131319baacf90b2c6035bdadb81
CodeWithNed <nadunk.20@cse.mrt.ac.lk>
2025-01-07 11:34:05 +0530

    javaEE http param calculator

---
+++ Project1/src/main/java/api/Student.java
@@ -0,0 +1,28 @@
+package api;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class Student extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        int  x = Integer.parseInt(req.getParameter("x"));
+        int  y = Integer.parseInt(req.getParameter("y"));
+        String operation = req.getParameter("o");
+
+        double result = switch (operation) {
+            case " " -> x + y;
+            case "-" -> x - y;
+            case "*" -> x * y;
+            case "/" -> y != 0 ? (double) x / y : Double.NaN;
+            default -> Double.NaN;
+        };
+
+        resp.getWriter().println("x " + operation + " y = " + result);
+    }
+}
